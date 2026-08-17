268295d92f736dab1ac9c734e70bd14eaddc1b2a
LerainzerGit <therecroommoviebackintime@gmail.com>
2026-08-17 17:00:08 +0100

    Create sysinfo.cs

---
+++ apps/sysinfo/sysinfo.cs
@@ -0,0 +1,15 @@
+using System;
+
+class SysInfo
+{
+    static void Main()
+    {
+        Console.WriteLine("Project Oreo System Info");
+        Console.WriteLine("------------------------");
+        Console.WriteLine("OS: Project Oreo");
+        Console.WriteLine("Kernel: Minimal Linux Base");
+        Console.WriteLine("Runtime: Mono");
+        Console.WriteLine("Architecture: x86");
+        Console.WriteLine("Apps Installed: miniedit, snake, sysinfo");
+    }
+}
